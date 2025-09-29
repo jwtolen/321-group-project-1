bb0cc3adc22cfda1903238aaa24dad799e1735cd
John Tolen <jwtolen@crimson.ua.edu>
2025-09-29 15:47:46 -0500

    it would be so cool if this worked

--- Client/Resources/Scripts/simple_test.js
+++
@@ -1,9 +0,0 @@
-console.log('Simple test script loading');
-alert('Simple test script is working!');
-
-window.handleRemoveListing = function() {
-  console.log('Simple handleRemoveListing called');
-  alert('Simple delete function works!');
-};
-
-console.log('Simple script loaded successfully');
